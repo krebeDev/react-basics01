55a0d8ffae69dc5bee46262f1187a3366b8c908c
Solomon Ekrebe <krebedev@gmail.com>
2020-01-16 10:17:51 +0100

    Updates

---
+++ precache-manifest.ce07c7ed464761032201944640ad4d86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ada58b9c2457e579ac0dc9730df23f1",
+    "url": "/react-basics01/index.html"
+  },
+  {
+    "revision": "97465c42b6509223dbeb",
+    "url": "/react-basics01/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "ca39713f51c7411ae8fc",
+    "url": "/react-basics01/static/js/2.29166a65.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-basics01/static/js/2.29166a65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97465c42b6509223dbeb",
+    "url": "/react-basics01/static/js/main.4ee564ab.chunk.js"
+  },
+  {
+    "revision": "ee98ffec6a8564148fd6",
+    "url": "/react-basics01/static/js/runtime-main.b486b0c5.js"
+  }
+]);
